041686d8562c8b6a039ccc1a73140b42a064c02a
Safa Bayraktar <SBayraktar@my.gcu.edu>
2022-02-06 15:30:35 -0700

    Google Maps Page Fixed!

--- src/MapContainer.js
+++ src/MapContainer.js
@@ -1,29 +1,48 @@
 import React from 'react';
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
-import LocationService from "./services/LocationService";
+import axios from "axios"
 
-const service = new LocationService();
+
+// Google's Map style property
 const containerStyle = {
     width: '1200px',
     height: '600px'
 };
 
+// default center (GCU Campus) defined for map initialize
 const center = {
     lat: 33.512743,
     lng: -112.130363
 };
 
-export class MapContainer extends React.Component {
-    state = { location: { lat: 33.510726, lng: -112.127271 } }
 
-    //  After 10 seconds - updates the location in state, crashes
-    //  cannot modify the defaultLocation of map...
-    componentDidMount() {
-        setTimeout(() => {
-            this.setState({ location: service.getLocation() })
-            console.log("Component mount: ", this.state.location);
-        }, 10000)
+export class MapContainer extends React.Component {
+    state = { location: { lat: 33.510726, lon: -112.127271 } }
+
+    // GET Request from SofanaGSP-API to get last location of the golf cart
+    // Updates the state to refresh map view
+    getLocation = async () => {
+        await axios.get('https://sofanagpsapi.azurewebsites.net/api/locations/lastLocation', {
+            auth: {
+                username: "sofanagps452",
+                password: "cst452ana"
+            }
+        })
+        .then(response => this.setState({ location: response.data }))
+    }  
+
+    async componentDidMount() {
+        await this.getLocation();
+        console.log("Component Mounted: ", this.state.location);
     }
+
+    componentDidUpdate() {
+        setTimeout( async () => {
+            await this.getLocation();
+            console.log("Component Updated: ", this.state.location);
+        }, 10000);
+    }
+
     render() {
         return (
             <div className="map container">
@@ -35,11 +54,9 @@ export class MapContainer extends React.Component {
                         <GoogleMap
                             mapContainerStyle={containerStyle}
                             center={center}
-                            zoom={10}
+                            zoom={16}
                         >
-                            <Marker position={{lat: this.state.location.lat, lng: this.state.location.lng}}/>
-                            <Marker position={{lat: 33.512738, lng: -112.131661}}/>
-                            
+                            <Marker position={{ lat: parseFloat(this.state.location.lat), lng: parseFloat(this.state.location.lon) }} />
                         </GoogleMap>
                     </LoadScript>
                 </div>

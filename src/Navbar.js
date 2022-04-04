410ed74bb4a1889217f042bff7406d72c4aa6bbb
Safa Bayraktar <SBayraktar@my.gcu.edu>
2022-04-03 17:56:17 -0700

    Responsive Navbar - Hamburger icon fixed!

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,15 +3,30 @@ import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
 
+    state={
+        showCollapsed: false
+    }
+
+    show = ""
+
+    //Responsive Hamburger Icon Functionality for React
+    toggleMenu = () => {
+        this.setState({
+            showCollapsed: !this.state.showCollapsed
+          })
+        this.show = (this.state.showCollapsed) ? "" : " show" ;
+    }
+
+
     render() {
         return(
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     <span className="navbar-brand" href="/" >SofanaGPS</span>
-                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    <button className="navbar-toggler" type="button" onClick={this.toggleMenu} data-toggle="collapse" data-target="#navbarNav"
                         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse" id="navbarNav">
+                    <div className={"collapse navbar-collapse" + this.show} id="navbarNav">
 
                         <ul className="navbar-nav">
                             <li className="nav-item">

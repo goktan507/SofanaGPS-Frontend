91a4c97ff7b076b93ef4530b009c4a6be4998678
Safa Bayraktar <SBayraktar@my.gcu.edu>
2022-04-06 16:15:42 -0700

    Logo added!

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,20 +1,24 @@
 import React from "react";
 import './style.css';
+import logo from "./assets/img/logo1.png";
 
 class AboutUs extends React.Component {
 
     render() {
-        return(
-            <div className="container">
-                <h1>SofanaGPS Team Members:</h1>
-                <ul>
-                    <li>
-                        Safa Bayraktar
-                    </li>
-                    <li>
-                        Ana Sanchez Sanchez
-                    </li>
-                </ul>
+        return (
+            <div className="container-2">
+                <img src={logo} alt="logo" className="logo" />
+                <div>
+                    <h1>SofanaGPS Team Members:</h1>
+                    <ul>
+                        <li>
+                            Safa Bayraktar
+                        </li>
+                        <li>
+                            Ana Sanchez Sanchez
+                        </li>
+                    </ul>
+                </div>
             </div>
         );
     }
